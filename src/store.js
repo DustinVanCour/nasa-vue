e8493e6ff6bab9680fe7d141968c3f30e8f2ec9a
Dustin VanCour <dustin82@gmail.com>
2019-06-05 08:54:53 -0600

    Good to go!

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+const _api = axios.create({
+  baseURL: "https://api.nasa.gov/planetary"
+
+})
+let apiKey = "apod?api_key=MM9caXS9DEOuzb1NRrY6M9k74FFKBeErlhfddBOm&date="
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    image: {}
   },
   mutations: {
+    setImage(state, data) {
+      state.image = data
+    }
 
   },
   actions: {
+    searchApi({ commit, dispatch }, query) {
+      _api.get(apiKey + query)
+        .then(res => {
+          let data = res.data
+          commit('setImage', data)
+        })
+    }
 
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="home">
-
+    <div class="row">
+      <div class="col">
+        <h1>Nasa Photo Fun!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+      </div>
+      <div class="col-auto">
+        <form @submit.prevent="searchPhotos">
+          <div class="form-row">
+            <div class="col-auto">
+              <input class="form-control" type="date" placeholder="" v-model="query"></div>
+            <div class="col-auto">
+              <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col" id="card-area">
+        <div class="card" style="width: 50rem;">
+          <img :src="image.url" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h3 class="card-title"><strong>{{image.title}}</strong></h3>
+            <p class="card-text">{{image.explanation}}}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +41,41 @@
 
   export default {
     name: 'home',
+    data() {
+      return {
+        query: ""
+      }
+    },
+    methods: {
+      searchPhotos() {
+        this.$store.dispatch("searchApi", this.query);
+      }
+    },
     components: {
 
-    }
+    },
+    computed: {
+      image() {
+        return this.$store.state.image
+      }
+    },
   }
 </script>
+
+<style>
+  h1 {
+    font-family: 'Baloo Da', cursive;
+    text-align: center;
+  }
+
+  form {
+    font-family: 'Baloo Da', cursive;
+    margin-bottom: 1rem;
+  }
+
+  #card-area {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
